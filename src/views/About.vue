aa48d52ed8a253e821e22f5f149f89753696c3a1
fangmingdong <fangmingdong0224@gmail.com>
2019-05-09 15:00:42 +0800

    01less配置及样式相关

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <p class="content">less test</p>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.about {
+  .content {
+    color: @title-color;
+  }
+}
+</style>
