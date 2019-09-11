f87207fec80d1254d8982f7e5ec166d2b9cb6fd7
fangmingdong <fangmingdong0224@gmail.com>
2019-09-11 16:51:13 +0800

    docker vue

---
+++ docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "start npm run install & npm run build"
+
+npm run install & npm run build
+
+container=$(docker container ls -a | grep vuenginxnew)
+if [ -n "$container" ]; then
+    echo "container is existed, restart container"
+    docker stop vuenginxnew
+    docker start vuenginxnew 
+else
+    echo "container not exist , create & start container success"
+    docker run \
+    -p 3000:80 \
+    -d --name vuenginxnew \
+    --mount type=bind,source=/Users/double/webWorkSpace/pvue/nginx,target=/etc/nginx/conf.d \
+    --mount type=bind,source=/Users/double/webWorkSpace/pvue/dist,target=/usr/share/nginx/html \
+    nginx
+fi
+
+
+
+
