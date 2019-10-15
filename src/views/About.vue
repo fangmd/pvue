da92de3d3c00b378ce7ecd92df38c7196e7c0b22
fangmingdong <fangmingdong0224@gmail.com>
2019-10-15 16:37:43 +0800

    add docker

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,20 +3,39 @@
     <h1>This is an about page</h1>
 
     <p class="content">less test</p>
+    <p class="content">{{cnt}}</p>
+
+    <div class="btn" @click="debounce">click test debounce</div>
+
+    <div class="btn" @click="throttles">click test throttles</div>
   </div>
 </template>
 
 <script>
 import { common } from '@/api';
+import _ from 'lodash';
+
 export default {
+  data() {
+    return {
+      cnt: 0,
+    };
+  },
   mounted() {
     const a = 'asd';
-    console.log('mounted ---- ');
-
     common
       .getUser()
       .then(resp => {})
       .catch(error => {});
+  },
+  methods: {
+    debounce: _.debounce(function() {
+      console.log('debounce');
+    }, 3000),
+    throttles: _.throttle(function() {
+      this.cnt++;
+      console.log('throttles' + this.cnt);
+    }, 3000),
   },
 };
 </script>
@@ -28,6 +47,16 @@ export default {
   }
   .content {
     color: @title-color;
+  }
+
+  .btn {
+    height: 50 / @q;
+    font-size: 30 / @q;
+    color: #000;
+
+    &:hover {
+      cursor: pointer;
+    }
   }
 }
 </style>
