b64a5a7ca9a4ce50cdffa29bbf86d12f79a82025
fangmingdong <fangmingdong0224@gmail.com>
2020-07-01 11:36:56 +0800

    upgrade config

--- src/router.js
+++ src/router.js
@@ -4,6 +4,16 @@ import Home from './views/Home.vue';
 
 Vue.use(Router);
 
+// 解决ElementUI导航栏中的vue-router在3.0版本以上重复点菜单报错问题
+// const routerPush = Router.prototype.push;
+// Router.prototype.push = function push(location) {
+//   return routerPush.call(this, location).catch(error => error);
+// };
+// const routerReplace = Router.prototype.replace;
+// Router.prototype.replace = function replace(location) {
+//   return routerReplace.call(this, location).catch(error => error);
+// };
+
 export default new Router({
   routes: [
     {
--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,21 @@ const CompressionPlugin = require('compression-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
-const selfSigned = require('openssl-self-signed-certificate');
+// const selfSigned = require('openssl-self-signed-certificate');
 
 module.exports = {
   //publicPath: '/dist/',
   assetsDir: 'static',
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .test(/\.(png|jpg|jpeg|gif|svg|svgz)$/i)
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({
+        bypassOnDebug: true,
+      });
+  },
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       if (process.env.ANALYZ_ENV) {
@@ -46,14 +56,13 @@ module.exports = {
     },
   },
   devServer: {
-    open: process.platform === 'darwin',
     host: '0.0.0.0',
     port: 8012,
-    https: {
-      key: selfSigned.key,
-      cert: selfSigned.cert,
-    },
-    hotOnly: false,
+    // https: {
+    //   key: selfSigned.key,
+    //   cert: selfSigned.cert,
+    // },
+    // hotOnly: false,
     proxy: {
       '/api': {
         target: 'http://接口服务器地址',
