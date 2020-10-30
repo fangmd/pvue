fdf444872438580dca64bb320d3989e1da3918f2
fangmingdong <fangmingdong0224@gmail.com>
2020-10-30 16:16:05 +0800

    code formate

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,30 @@
-const path = require('path');
-const CompressionPlugin = require('compression-webpack-plugin');
-const TersetPlugin = require('terser-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path')
+const CompressionPlugin = require('compression-webpack-plugin')
+const TersetPlugin = require('terser-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 // const selfSigned = require('openssl-self-signed-certificate');
 
 function getGitVersionInfo() {
   if (process.env.NODE_ENV === 'production') {
     try {
-      let fs = require('fs');
-      let gitHEAD = fs.readFileSync('.git/HEAD', 'utf-8').trim(); // ref: refs/heads/master
-      let ref = gitHEAD.split(': ')[1]; // refs/heads/master
-      let branchName = gitHEAD.split('/')[2]; // master
-      let gitVersion = fs.readFileSync('.git/' + ref, 'utf-8').trim(); // git版本号，例如：db3b1d0e91f41026ebf50fc20a17df8a5317dd57
-      let gitCommitVersion = '"' + branchName + ': ' + gitVersion + '"'; // 例如dev环境: "master: db3b1d0e91f41026ebf50fc20a17df8a5317dd57"
-      return gitCommitVersion;
+      let fs = require('fs')
+      let gitHEAD = fs.readFileSync('.git/HEAD', 'utf-8').trim() // ref: refs/heads/master
+      let ref = gitHEAD.split(': ')[1] // refs/heads/master
+      let branchName = gitHEAD.split('/')[2] // master
+      let gitVersion = fs.readFileSync('.git/' + ref, 'utf-8').trim() // git版本号，例如：db3b1d0e91f41026ebf50fc20a17df8a5317dd57
+      let gitCommitVersion = '"' + branchName + ': ' + gitVersion + '"' // 例如dev环境: "master: db3b1d0e91f41026ebf50fc20a17df8a5317dd57"
+      return gitCommitVersion
     } catch (error) {
-      console.log(error);
-      return '';
+      console.log(error)
+      return ''
     }
   } else {
-    return '';
+    return ''
   }
 }
 
 //用于标记项目发布时间
-process.env.VUE_APP_BUILDTIME = new Date().toString() + getGitVersionInfo();
+process.env.VUE_APP_BUILDTIME = new Date().toString() + getGitVersionInfo()
 
 module.exports = {
   //publicPath: '/dist/',
@@ -37,12 +37,12 @@ module.exports = {
       .loader('image-webpack-loader')
       .options({
         bypassOnDebug: true,
-      });
+      })
   },
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       if (process.env.ANALYZ_ENV) {
-        config.plugins.push(new BundleAnalyzerPlugin());
+        config.plugins.push(new BundleAnalyzerPlugin())
       }
       //压缩插件
       config.plugins.push(
@@ -51,7 +51,7 @@ module.exports = {
           threshold: 10240, //对超过10k的数据压缩
           deleteOriginalAssets: false, //不删除源文件
         })
-      );
+      )
       config.optimization.minimizer.push(
         new TersetPlugin({
           terserOptions: {
@@ -64,9 +64,9 @@ module.exports = {
           sourceMap: false,
           parallel: true,
         })
-      );
+      )
     } else {
-      config.devtool = 'source-map';
+      config.devtool = 'source-map'
     }
   },
   productionSourceMap: false,
@@ -91,4 +91,4 @@ module.exports = {
     },
     before: process.env.VUE_APP_MOCK === 'true' ? require('./mock') : null,
   },
-};
+}
